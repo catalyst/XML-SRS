d33e24443975ca19d44870df88ac8998bda2f7d5
Andrew Chilton <andychilton@gmail.com>
2010-01-05 11:09:49 +1300

    wr65215 - belated class for Domain::Create (should have been with 19ead0b4)

---
+++ lib/XML/SRS/Domain/Create.pm
@@ -0,0 +1,69 @@
+package XML::SRS::Domain::Create;
+
+use Moose;
+use PRANG::Graph;
+use XML::SRS::Types;
+use XML::SRS::Server::List;
+
+has_attr 'domain_name' =>
+	is => "ro",
+	isa => "XML::SRS::DomainName",
+	xml_name => "DomainName",
+    required => 1,
+	;
+
+has_attr 'registrant_ref' =>
+	is => "ro",
+	isa => "XML::SRS::UID",
+	xml_name => "RegistrantRef",
+	xml_required => 0,
+	;
+
+has_attr 'term' =>
+	is => "ro",
+	isa => "XML::SRS::Term",
+	xml_name => "Term",
+    required => 1,
+	;
+
+has_attr 'delegate' =>
+	is => "ro",
+	isa => "XML::SRS::Boolean",
+	xml_name => "Delegate",
+	xml_required => 0,
+	;
+
+has_element 'contact_registrant' =>
+	is => "ro",
+	isa => "XML::SRS::Contact",
+	xml_nodeName => "RegistrantContact",
+    required => 1,
+	;
+
+has_element 'contact_admin' =>
+	is => "ro",
+	isa => "XML::SRS::Contact",
+	xml_nodeName => "AdminContact",
+	xml_required => 0,
+	;
+
+has_element 'contact_technical' =>
+	is => "ro",
+	isa => "XML::SRS::Contact",
+	xml_nodeName => "TechnicalContact",
+	xml_required => 0,
+	;
+
+has_element "nameservers" =>
+	is => "ro",
+	isa => "XML::SRS::Server::List",
+	xml_nodeName => "NameServers",
+	xml_required => 0,
+	;
+
+with 'XML::SRS::Audit';
+
+sub root_element { 'DomainCreate' }
+with 'XML::SRS::Action';
+
+1;

2576f30cef406847f826c7b2c43c7c8c9c377f6d
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-03-30 18:51:21 +1300

    Begin man page for XML::SRS
    More to come!

--- lib/XML/SRS.pm
+++ lib/XML/SRS.pm
@@ -41,3 +41,49 @@ use XML::SRS::Registrar;
 use XML::SRS::UDAIValid;
 
 1;
+
+__END__
+
+=head1 NAME
+
+XML::SRS - Shared Registry System XML Protocol
+
+=head1 SYNOPSIS
+
+ my $create = XML::SRS::Domain::Create->new(
+          domain_name => "thinkbig.co.nz",
+          term => 12,
+          delegate => 1,
+          contact_registrant => {
+               name => "Robert Muldoon",
+               email => 'get.drunk.and.call.a.snap.election@gmail.com',
+               address => {
+                    address1 => "57 Mount Pleasant St",
+                    address2 => "Burbia",
+                    city => "Ohai",
+                    region => "Southland",
+                    cc => "NZ",
+               },
+               phone => {
+                    cc => "64",
+                    ndc => "4",
+                    subscriber => "499 2267",
+               },
+          },
+          nameservers => [qw( ns1.registrar.net.nz ns2.registrar.net.nz )],
+         );
+
+  my $xml_request = $create->to_xml;
+
+=head1 DESCRIPTION
+
+This module is an implementation of the XML protocol used by the .nz
+registry.  This protocol was developed in 2002, using what were
+considered stable XML standard methods at the time, such as SGML DTD.
+This compares to the now de-facto standard, EPP (RFC3730, friends and
+updates), which was developed using XML Schema and XML Namespaces.  As
+such, the SRS protocol as a stable standard far pre-dates EPP, which
+took a further 2 years to reach 1.0 status.
+
+=cut
+

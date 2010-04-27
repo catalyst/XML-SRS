0c228cc162d02086403afda109ba9c7371637e44
Sam Vilain <sam@vilain.net>
2010-04-28 09:43:59 +1200

    Coerce rules for coercing passwords from (possibly crypted) strings

--- lib/XML/SRS/Password.pm
+++ lib/XML/SRS/Password.pm
@@ -3,14 +3,27 @@ package XML::SRS::Password;
 
 use Moose;
 use PRANG::Graph;
+use Crypt::Password;
+use Moose::Util::TypeConstraints;
+
+class_type "Crypt::Password";
 
 has_attr 'crypted' =>
 	is => "ro",
-	isa => "Str",
+	isa => "Str|Crypt::Password",
 	xml_name => "Password",
 	required => 1,
 	;
 
 with 'XML::SRS::Node';
 
+coerce __PACKAGE__
+	=> from "Str"
+	=> via {
+		__PACKAGE__->new(crypted => password($_));
+	},
+	=> from "Crypt::Password"
+	=> via {
+		__PACKAGE__->new(crypted => password($_));
+	};
 1;

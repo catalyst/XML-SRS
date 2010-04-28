bea182de03dbd2e30606dbcc3f670c84e5f6cd82
Sam Vilain <sam.vilain@catalyst.net.nz>
2010-04-28 18:59:51 +1200

    Fiddle with Password - make 'crypted' always a Crypt::Password object

--- lib/XML/SRS/Password.pm
+++ lib/XML/SRS/Password.pm
@@ -10,10 +10,18 @@ class_type "Crypt::Password";
 
 has_attr 'crypted' =>
 	is => "ro",
-	isa => "Str|Crypt::Password",
+	isa => "Crypt::Password",
+	handles => [qw/check/],
 	xml_name => "Password",
 	required => 1,
+	coerce => 1,
 	;
+
+coerce "Crypt::Password"
+	=> from "Str"
+	=> via {
+		password($_);
+	};
 
 with 'XML::SRS::Node';
 
@@ -24,6 +32,6 @@ coerce __PACKAGE__
 	},
 	=> from "Crypt::Password"
 	=> via {
-		__PACKAGE__->new(crypted => password($_));
+		__PACKAGE__->new(crypted => $_);
 	};
 1;

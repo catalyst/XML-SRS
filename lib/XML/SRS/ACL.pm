abc298b20413307bb3617bd62099c3647126faea
Sam Crawley <samc@catalyst.net.nz>
2010-07-28 14:12:55 +1200

    ACLEntry is not required in ACLQry responses

--- lib/XML/SRS/ACL.pm
+++ lib/XML/SRS/ACL.pm
@@ -46,7 +46,7 @@ has_element 'entries' =>
 	is => "ro",
 	isa => "ArrayRef[XML::SRS::ACL::Entry]",
 	xml_nodeName => "AccessControlListEntry",
-	required => 1,
+	xml_required => 0,
 	;
 
 sub root_element {

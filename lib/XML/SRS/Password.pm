0e55ca69b5e3dea0cef04faac4e244163bff6f53
Sam Crawley <samc@catalyst.net.nz>
2011-02-14 15:56:19 +1300

    wr77086: remove Crypt::Password, as it is not platform independent.
    Crypt Password uses "Modular Crypt Format", which is specific to platforms
    (at least is different on linux and bsd). This makes it very difficult for
    clients to construct encrypted passwords that the SRS can make use of. It
    also makes this module difficult to install on non-linux platforms. However,
    since the SRS supports plain-text passwords (which it then converts itself),
    we can just remove Crypt::Password altogether.

--- lib/XML/SRS/Password.pm
+++ lib/XML/SRS/Password.pm
@@ -3,35 +3,20 @@ package XML::SRS::Password;
 
 use Moose;
 use PRANG::Graph;
-use Crypt::Password;
 use Moose::Util::TypeConstraints;
 
-class_type "Crypt::Password";
-
-coerce "Crypt::Password"
-	=> from "Str"
-	=> via {
-	password($_);
-	};
 
 has_attr 'crypted' =>
 	is => "ro",
-	isa => "Crypt::Password",
-	xml_isa => "Str",
-	handles => [qw/check/],
+	isa => "Str",
 	xml_name => "Password",
 	required => 1,
-	coerce => 1,
 	;
 
 with 'XML::SRS::Node';
 
 coerce __PACKAGE__
 	=> from "Str"
-	=> via {
-	__PACKAGE__->new(crypted => password($_));
-	},
-	=> from "Crypt::Password"
 	=> via {
 	__PACKAGE__->new(crypted => $_);
 	};

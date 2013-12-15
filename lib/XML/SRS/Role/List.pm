800b93bd8d7ce37c3806b05355038e689faa4f7f
Sam Crawley <samc@catalyst.net.nz>
2013-12-16 10:58:44 +1300

    wr217292: added some coercion rules to zone/role list

--- lib/XML/SRS/Role/List.pm
+++ lib/XML/SRS/Role/List.pm
@@ -7,11 +7,25 @@ use XML::SRS::Role;
 
 use Moose::Util::TypeConstraints;
 
+subtype 'XML::SRS::RoleArrayRef',
+    as "ArrayRef[XML::SRS::Role]";
+
+coerce "XML::SRS::RoleArrayRef"
+    => from 'ArrayRef[Str]'
+    => via {
+        [
+            map {
+                XML::SRS::Role->new(RoleName => $_);
+                } @$_
+        ],
+    };
+
 has_element 'roles' =>
 	is => "rw",
-	isa => "ArrayRef[XML::SRS::Role]",
+	isa => "XML::SRS::RoleArrayRef",
 	xml_nodeName => "Role",
 	xml_required => 0,
+	coerce => 1,
 	;
 
 coerce __PACKAGE__
--- lib/XML/SRS/Zone/List.pm
+++ lib/XML/SRS/Zone/List.pm
@@ -7,11 +7,25 @@ use XML::SRS::Zone;
 
 use Moose::Util::TypeConstraints;
 
+subtype 'XML::SRS::ZoneArrayRef',
+    as "ArrayRef[XML::SRS::Zone]";
+
+coerce "XML::SRS::ZoneArrayRef"
+    => from 'ArrayRef[Str]'
+    => via {
+        [
+            map {
+                XML::SRS::Zone->new(DomainName => $_);
+                } @$_
+        ],
+    };
+
 has_element 'zones' =>
 	is => "rw",
-	isa => "ArrayRef[XML::SRS::Zone]",
+	isa => "XML::SRS::ZoneArrayRef",
 	xml_nodeName => "SecondLD",
-	xml_required => 0,	
+	xml_required => 0,
+	coerce => 1,
 	;
 
 coerce __PACKAGE__

2c5231dc13f4b01e28275da765402e16f8954e11
Sam Crawley <samc@catalyst.net.nz>
2013-12-18 11:59:41 +1300

    wr217283: removed incorrect validation rule from ACL

--- lib/XML/SRS/ACL.pm
+++ lib/XML/SRS/ACL.pm
@@ -32,12 +32,6 @@ has_attr 'SizeChange' =>
 	xml_required => 0,
 	;
 
-sub BUILD {
-	my $self = shift;
-	defined($self->Size//$self->SizeChange)
-		or die "Must specify either Size or SizeChange";
-}
-
 has_attr 'Type' =>
 	is => "ro",
 	isa => "Str",

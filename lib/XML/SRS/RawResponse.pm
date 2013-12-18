2cd4afc95a98d030a2e3db5b01cda261cce11136
Sam Crawley <samc@catalyst.net.nz>
2013-12-18 17:34:24 +1300

    wr217283: RawResponse/RawRequest attributes are not always required

--- lib/XML/SRS/RawResponse.pm
+++ lib/XML/SRS/RawResponse.pm
@@ -7,7 +7,6 @@ has_element 'xml' =>
     is => "rw",
     isa => "Str",
     xml_nodeName => "XML",
-    required => 1,
     ;
 
 
@@ -15,7 +14,6 @@ has_element 'signature' =>
     is => "rw",
     isa => "Str",
     xml_nodeName => "Signature",
-    required => 1,
     ;
 
 with 'XML::SRS::ActionResponse';

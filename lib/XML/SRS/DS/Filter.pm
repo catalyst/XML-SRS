7bc9c21971aad55b2b24b4a1790e4fd1f3c5fe71
Sam Crawley <samc@catalyst.net.nz>
2013-12-18 13:46:38 +1300

    wr217283: DS Filter attributes are not required

--- lib/XML/SRS/DS/Filter.pm
+++ lib/XML/SRS/DS/Filter.pm
@@ -4,4 +4,20 @@ use Moose;
 
 extends 'XML::SRS::DS';
 
+has '+key_tag' =>
+    required => 0,
+    ;
+
+has '+algorithm' =>
+    required => 0,
+    ;
+
+has '+digest_type' =>
+    required => 0,
+    ;
+
+has '+digest' =>
+    required => 0,
+    ;
+
 1;
